1b5a2b9f97a7f1ff2d04734955e1f5151be05b9c
alpsjur <alpsjur@runbox.com>
2024-03-15 14:27:17 +0100

    pipeline momentum_terms

--- postprocessing/functions.py
+++ postprocessing/functions.py
@@ -39,19 +39,138 @@ def center2yface(da):
 
     return day
 
-def momentumAdvection(u,v):
+def momentum_advection(u,v):
     return u*v
 
-def centerDepthIntegral(var, dz):
-    return (var*dz).sum("sigma")
+def depth_integral(var, dz, zcoord="sigma"):
+    return (var*dz).sum(zcoord)
 
-def xfaceDepthIntegral(var, dz):
-    dzx = center2xface(dz)
-    return (var*dzx).sum("sigma")
-
-def yfaceDepthIntegral(var, dz):
-    dzy = center2yface(dz)
-    return (var*dzy).sum("sigma")
-
-def timeDerivative(var):
+def time_derivative(var):
     return var.differentiate("time", datetime_unit="s")
+
+def total_depth(ds):
+    return (depth_integral(1,ds.dz) - ds.sealv).isel(time=0)
+
+def pad_reentrance(var):
+    return xr.concat([var.isel(x=-1), var, var.isel(x=0)], dim="x")
+
+# from ubbl.py
+# dette forstår jeg ikke helt. Hva gjør np.where i dette tilfellet? Hvorfor gir den en liste med 34 arrays, der vi bare bruker første?
+def _bottom_value(u,dz):
+    bi=np.where(dz>1)[0]
+    if len(bi>0):
+        return u[bi[-1]]
+    else:
+        return np.nan
+    
+def calculate_dhdx(ds, dx):
+    # calculate depth H. Total water height - sea surface elevation
+    H = total_depth(ds)
+
+    # pad H in reentranse direction
+    Hpad = pad_reentrance(H).chunk({"x":-1, "y":-1})
+
+    # Calculate derivative of bottom height using central difference    
+    dhdx = -Hpad.differentiate("x").isel(x=slice(1,-1))/dx
+    
+    return dhdx
+
+def dUVdy(ds, dy):
+    # calculate momentum flux divergence, second order difference
+    uv = momentum_advection(ds.uc,ds.vc)
+    UV = depth_integral(uv, ds.dz)
+    dUVdy = UV.differentiate("y")/dy
+    
+    return dUVdy
+
+
+def get_bottom_value(var, dz, zcoord="sigma"):
+    varb = xr.apply_ufunc(_bottom_value, var, dz,
+                                      input_core_dims=[[zcoord],[zcoord]],
+                                      output_core_dims=[[]],
+                                      vectorize=True,
+                                      dask='parallelized',
+                                      output_dtypes=[var.dtype])
+    
+    return varb
+
+def _calculate_bottomdrag(ub, vb):
+    # bottom drag coefficintes
+    cbar = 0.05 # is RMS flow speed for linear bottom friction law in [m s-1].
+    cb=0.002  # is Coefficient of quadratic bottom friction [unitless].
+    q = cb*(np.sqrt(ub**2+vb**2)+cbar)
+    tauxb = ub*q
+    return tauxb
+
+
+def add_coordinate_values(ds, dx, dy):
+    ds["x"] = dx*np.arange(0,len(ds.x))
+    ds["y"] = dy*np.arange(0,len(ds.y))
+    
+def _dUdt_center_first(ds):
+    uc = ds.uc
+    dz = ds.dz
+    U = depth_integral(uc, dz)
+    dUdt = time_derivative(U)
+    return dUdt
+
+def _dUdt_center_last(ds):
+    uc = ds.uc
+    dzx = ds.dzx
+    U = depth_integral(uc, dzx)
+    dUdt = time_derivative(U)
+    return xface2center(dUdt)
+    
+def dUdt(ds, method="center first"):
+    if method == "center first":
+        return _dUdt_center_first(ds)
+    elif method == "center last":
+        return _dUdt_center_last(ds)
+    else:
+        raise ValueError("Method must be either ""center first"" or ""center last""")
+    
+def _fV_center_first(ds, f0):
+    vc = ds.vc
+    dz = ds.dz
+    return depth_integral(f0*vc, dz)
+
+def _fV_center_last(ds, f0):
+    v = ds.v
+    dzy = ds.dzy
+    return yface2center(depth_integral(f0*v,dzy))
+    
+def fV(ds, f0=1e-4, method="center first"):
+    if method == "center first":
+        return _fV_center_first(ds, f0)
+    elif method == "center last":
+        return _fV_center_last(ds, f0)
+    else:
+        raise ValueError("Method must be either ""center first"" or ""center last""")
+
+
+def phidhdx(ds, rho=1e3, dx=2e3):
+    dhdx = calculate_dhdx(ds, dx)
+    return ds.pbot*dhdx/rho
+
+def ubar(ds):
+    return xface2center(ds.ubaro)
+
+def _tauxb_center_first(ds):
+    ucb = get_bottom_value(ds.uc,ds.dz)
+    vcb = get_bottom_value(ds.vc,ds.dz)
+    return _calculate_bottomdrag(ucb, vcb)
+
+def _tauxb_center_last(ds):
+    ub = get_bottom_value(ds.u,ds.dzx)
+    vb = get_bottom_value(ds.v,ds.dzy)
+    ucb = xface2center(ub)
+    vcb = yface2center(vb)
+    return _calculate_bottomdrag(ucb, vcb)
+
+def tauxb(ds, method="center first"):
+    if method == "center first":
+        return _tauxb_center_first(ds)
+    elif method == "center last":
+        return _tauxb_center_last(ds)
+    else:
+        raise ValueError("Method must be either ""center first"" or ""center last""")
--- postprocessing/momentum_terms.py
+++ postprocessing/momentum_terms.py
@@ -1,7 +1,7 @@
 import xarray as xr
 import numpy as np
 from dask.distributed import Client
-import os
+from pathlib import Path
 import functions as f
 
 #datapath = "/projects/NS9869K/noresm/cases/BLOM_channel/"
@@ -10,7 +10,12 @@ datapath = "/projects/NS9252K/noresm/cases/BLOM_channel/"
 #case = "BLOM_channel_new05_mix1_taupos5"
 case = "BLOM_channel_new05_mix1"
 #case = "BLOM_channel_new02_mix1"
-outpath = "/nird/home/annals/BLOM_analysis/data/"+case+"/"
+
+method = "from_vel"
+#method = "from_flux"
+
+outpath = f"/nird/home/annals/BLOM_analysis/data/{case}/{method}/"
+Path(outpath).mkdir(parents=True, exist_ok=True)
 
 #client = Client(n_workers=2, threads_per_worker=2, memory_limit='1GB')
 
@@ -39,130 +44,63 @@ ds = xr.open_mfdataset(datapath+case+"/*hd_*.nc",
 # empty data set for storing processed data
 pds = xr.Dataset()
 
+dzx = f.center2xface(ds.dz)
+dzy = f.center2yface(ds.dz)
 
-u = ds.uvel
-v = ds.vvel
+if method == "from_vel":
+    u = ds.uvel
+    v = ds.vvel
+elif method == "from_flux":
+    uflx = ds.uflx 
+    Ax = dx*ds.dzx
+    u = uflx/(rho*Ax)
+    
+    vflx = ds.vflx 
+    Ay = dy*ds.dzy
+    v = vflx/(rho*Ay)
+    
 
 uc = f.xface2center(u)
 vc = f.yface2center(v)
 
+
 # store values 
+ds["u"] = u.chunk({"x":xchunk, "y":ychunk, "sigma":sigmachunk, "time":timechunk})
+ds["v"] = v.chunk({"x":xchunk, "y":ychunk, "sigma":sigmachunk, "time":timechunk})
 ds["uc"] = uc.chunk({"x":xchunk, "y":ychunk, "sigma":sigmachunk, "time":timechunk})
 ds["vc"] = vc.chunk({"x":xchunk, "y":ychunk, "sigma":sigmachunk, "time":timechunk})
+ds["dzx"] = dzx
+ds["dzy"] = dzy
 
 
-
-# calculate momentum advection
-ds["uv"] = f.momentumAdvection(u,v)
-
-
-# calculate depth integral of momentum advection and u velocity
-pds["UV"] = f.centerDepthIntegral(ds.uv, ds.dz)
-pds["U1"] = f.centerDepthIntegral(ds.uc, ds.dz)
-pds["U2"] = f.xfaceDepthIntegral(ds.u, ds.dz)
-
-# calculate time derivative of velocity, second order difference
-dUdt1 = f.timeDerivative(pds.U1)
-dUdt2 = f.timeDerivative(pds.U2)
-
-pds["dUdt1"] = dUdt1
-pds["dUdt2"] = xface2center(dUdt2)
+# time derivative of depth integrated zonal velocity
+pds["dUdt1"] = f.dUdt(ds, method="center last")
+pds["dUdt2"] = f.dUdt(ds, method="center first")
 
 
 # advection of planetary vorticity
-pds["fV1"] = f.centerDepthIntegral(f0*vc, dz) 
-pds["fV2"] = yface2center(f.yfaceDepthIntegral(f0*v,dz))
+pds["fV1"] = f.fV(ds, method="center last") 
+pds["fV2"] = f.fV(ds, method="center first") 
 
+# topographic form stress term
+pds["phidhdx"] = f.phidhdx(ds, rho, dx)
 
-# calculate depth H. Total water height - sea surface elevation
-H = (ds.dz.sum(dim = 'sigma') - ds.sealv).isel(time=0)#.mean("time")            # [m]
+# momentum flux divergence
+pds["dUVdy"] =  f.dUVdy(ds, dy)
 
-# pad H in reentranse direction
-Hpad = xr.concat([H.isel(x=-1), H, H.isel(x=0)], dim="x").chunk({"x":-1, "y":-1})
-#Hpad["x"] = dx*np.arange(-1,len(ds.x)+1)
-
-# Calculate derivative of bottom height using central difference    
-dhdx = -Hpad.differentiate("x").isel(x=slice(1,-1))/dx
-ds["dhdx"] = dhdx
-pds["phidhdx"] = ds.pbot*ds.dhdx/rho
-
-# calculate momentum flux divergence, second order difference
-dUVdy = pds.UV.differentiate("y")/dy
-pds["dUVdy"] =  dUVdy
-
-# calculate bottom drag
-cbar = 0.05 # is RMS flow speed for linear bottom friction law in [m s-1].
-cb=0.002  # is Coefficient of quadratic bottom friction [unitless].
-
-
-# from ubbl.py
-# dette forstår jeg ikke helt. Hva gjør np.where i dette tilfellet? Hvorfor gir den en liste med 34 arrays, der vi bare bruker første?
-def bottom_vel(u,dz):
-    bi=np.where(dz>1)[0]
-    if len(bi>0):
-        return u[bi[-1]]
-    else:
-        return np.nan
-
-
-ub = xr.apply_ufunc(bottom_vel, u, dzx,
-                                      input_core_dims=[['sigma'],['sigma']],
-                                      output_core_dims=[[]],
-                                      vectorize=True,
-                                      dask='parallelized',
-                                      output_dtypes=[u.dtype])
-    
-vb = xr.apply_ufunc(bottom_vel, v, dzx,
-                                  input_core_dims=[['sigma'],['sigma']],
-                                  output_core_dims=[[]],
-                                  vectorize=True,
-                                  dask='parallelized',
-                                  output_dtypes=[v.dtype])
-
-ubc1 = xface2center(ub)
-vbc1 = yface2center(vb)
-
-
-ubc2 = xr.apply_ufunc(bottom_vel, uc, dz,
-                                      input_core_dims=[['sigma'],['sigma']],
-                                      output_core_dims=[[]],
-                                      vectorize=True,
-                                      dask='parallelized',
-                                      output_dtypes=[uc.dtype])
-    
-vbc2 = xr.apply_ufunc(bottom_vel, vc, dz,
-                                  input_core_dims=[['sigma'],['sigma']],
-                                  output_core_dims=[[]],
-                                  vectorize=True,
-                                  dask='parallelized',
-                                  output_dtypes=[vc.dtype])
-
-
-
-
-q1 = cb*(np.sqrt(ubc1**2+vbc1**2)+cbar)
-tauxb1 = ubc1*q1
-
-pds["tauxb1"] = tauxb1
-
-q2 = cb*(np.sqrt(ubc2**2+vbc2**2)+cbar)
-tauxb2 = ubc2*q2
-
-pds["tauxb2"] = tauxb2
+# bottom drag
+pds["tauxb1"] = f.tauxb(ds, method="center last")
+pds["tauxb2"] = f.tauxb(ds, method="center last")
 
 # add mean zonal velocity
-pds["ubar"] = xface2center(ds.ubaro)
+pds["ubar"] = f.ubar(ds)
 
 # set values to coordinates. 
-pds["x"] = dx*np.arange(0,len(pds.x))
-pds["y"] = dy*np.arange(0,len(pds.y))
-
+f.add_coordinate_values(pds)
 
 # save bottom bathymetry
-bath = -H
-bath["x"] = dx*np.arange(0,len(bath.x))
-bath["y"] = dy*np.arange(0,len(bath.y))
-print(bath)
+bath = -f.total_depth(ds)
+f.add_coordinate_values(bath)
 bath.to_netcdf(outpath+case+"_bathymetry.nc")
 
 # zonal mean
@@ -177,6 +115,6 @@ nmonths = ntime//n
 for t in np.arange(nmonths):
     result = results.isel(time=slice(t*n,(t+1)*n))
     print(t)
-    result.to_netcdf(outpath+case+f"_momentumterms_{t:03}.nc")
+    result.to_netcdf(outpath+f"{case}_{method}_momentumterms_{t:03}.nc")
 
 #results.to_netcdf(outpath+case+"_momentumterms.nc")
